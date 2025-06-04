2dfa63fe7321f46ebada18871aebcd222c31ed0c
Prabesh Gyawali <prabeshgyawali928@gmail.com>
2025-06-04 18:38:52 -0400

    dmeo

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import PageWrapper from '../Common/PageWrapper';
 
 const Contact = () => {
+   const BASE_URL = import.meta.env.VITE_API_URL || '';
   const [form, setForm] = useState({
     name: '',
     email: '',
