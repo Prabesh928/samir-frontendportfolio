a96ed46f36c9c8152ec57bac6fc4814a543757e2
Prabesh Gyawali <prabeshgyawali928@gmail.com>
2025-06-04 17:26:13 -0400

    adding route

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -13,7 +13,7 @@ const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [showPassword, setShowPassword] = useState(false);
-  const BASE_URL = process.env.REACT_APP_API_URL || '';
+ const BASE_URL = import.meta.env.VITE_API_URL || '';
 
   let handellogin=async()=>{
 
